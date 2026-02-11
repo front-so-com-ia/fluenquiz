067801ff3eef7c1a81622229b0207acca7b4c27a
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2026-02-11 19:21:59 -0300

    feat: Implementa a base das páginas Home e Trivia

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import BaseContainer from '../../components/BaseContainer';
+
+export default function Home() {
+  return (
+    <BaseContainer>
+      <h1 className="text-black text-4xl font-bold text-center p-4">Home</h1>
+    </BaseContainer>
+  );
+}
